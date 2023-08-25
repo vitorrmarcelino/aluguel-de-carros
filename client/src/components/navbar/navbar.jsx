5353789ff3a406e7860229cd1c7b9ca31a8497e2
Vitor Rafael Marcelino <111241590+vitorrmarcelino@users.noreply.github.com>
2023-08-25 10:14:46 -0300

    inicio do projeto

---
+++ client/src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+import LogoCar from '../../assets/logo.webp'
+
+const navbar = () => {
+  return (
+    <header>
+        <img src={LogoCar} alt="" />
+    </header>
+  )
+}
+
+export default navbar
