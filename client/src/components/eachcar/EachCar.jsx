15d03decc2ba9c4489a35811db4ff6c59a7130cf
vitorrmarcelino <vitorrmarcelino09@gmail.com>
2023-09-01 19:45:11 -0300

    Inserindo todos os carros no front end

--- client/src/components/eachcar/EachCar.jsx
+++ client/src/components/eachcar/EachCar.jsx
@@ -1,32 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import propTypes from "prop-types";
 import "./style.css";
-import api from "../../api/Api";
 
-const EachCar = () => {
+const EachCar = ({ data }) => {
+  const { ImgLink, _id, carName, carBrand, engineDisplacementInMl, Automatic , isHatch, isSedan, isSUV} =
+    data;
 
-  api.get("carros").then((res) => {
-    console.log(res.data);
-  });
+  let gear;
 
-  const ImgLink ="https://file.kelleybluebookimages.com/kbb/base/evox/CP/12674/2019-Volkswagen-Golf%20GTI-front_12674_032_2400x1800_K8K8.png";
-  const id = "64f206a69db63087e8cc05ac";
+  if (Automatic === true) {
+    gear = "Câmbio Automático";
+  } else {
+    gear = "Câmbio Manual";
+  }
+
+  let carType;
+
+  if(isHatch===true){
+    carType = "Hatch"
+  }else if(isSedan === true){
+    carType = "Sedan"
+  }else if(isSUV ===true){
+    carType = "SUV"
+  }
 
   return (
     <div className="container">
       <img src={ImgLink} className="carImg" alt="car" />
       <h3>
-        <span>Marca do Carro</span>
-        <span> Nome do Carro</span>
+        <span>{carBrand}</span>
+        <span> {carName}</span>
       </h3>
-      <p>
-        <span>Motor do Carro</span>
-        <span> Automatico/manual</span>
-      </p>
-      <p>Categoria do Carro</p>
-      <Link to={`/alugar/${id}`}>Alugar</Link>
+      <span>Motor {engineDisplacementInMl}</span> <br />
+      <span>{gear}</span>
+      <p>{carType}</p>
+      <Link to={`/alugar/${_id}`}>Alugar</Link>
     </div>
   );
 };
 
 export default EachCar;
+
+EachCar.propTypes = {
+  data: propTypes.shape({}),
+}.isRequired;
--- client/src/pages/CarRent/CarRent.jsx
+++ client/src/pages/CarRent/CarRent.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import EachCar from "../../components/eachcar/EachCar";
+import api from "../../api/Api";
 
 const CarRent = () => {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    api.get("carros").then((res) => {
+      setCars(res.data);
+    });
+  }, []);
+
   return (
     <div className="container">
-      <EachCar />
+      {cars.map((car) => (
+        <EachCar key={car.id} data={car} />
+      ))}
     </div>
   );
 };
