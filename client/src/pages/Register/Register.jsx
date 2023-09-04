35fdf6063f5c7c2648d7efdc57ff086224452707
vitorrmarcelino <vitorrmarcelino09@gmail.com>
2023-09-04 19:09:21 -0300

    falta bastante

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import './Register.css'
+import React, { useState, useEffect } from "react";
+import "./Register.css";
+import PostRegisterUser from '../../api/PostRegisterUser';
 
 const Register = () => {
-  return (
-    <div className='container'>
-        <form action="/">
-            <label htmlFor="iname">
-                Nome: 
-            </label>
-                <input type="text" id='iname' />
-        </form>
-    </div>
-  )
-}
+  const [registerName, setRegisterName] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-export default Register
+  return (
+    <div className="container">
+      <form action="" method="POST">
+        <div>
+          <label htmlFor="iname">Nome:</label>
+          <input type="text" id="iname" name="name"/>
+        </div>
+        <div>
+          <label htmlFor="iemail">E-mail:</label>
+          <input type="email" id="iemail" name="email"/>
+        </div>
+        <div>
+          <label htmlFor="ipassword">Senha:</label>
+          <input type="password" id="ipassword" name="password"/>
+        </div>
+        <div>
+          <label htmlFor="iconfirmpassword">Confirmar Senha:</label>
+          <input type="password" id="iconfirmpassword" name="confirmpassword"/>
+        </div>
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
