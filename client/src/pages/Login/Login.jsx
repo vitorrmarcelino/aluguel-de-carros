51ee15b483cf3ea898985168e983deab6f49f2f6
vitorrmarcelino <vitorrmarcelino09@gmail.com>
2023-09-05 20:28:40 -0300

    registro feito

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,8 +1,25 @@
-import React from "react";
-import './Login.css'
+import React, { useEffect, useState } from "react";
+import "./Login.css";
 
 const Login = () => {
-  return <div className="container">login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="container">
+      <form action="" method="POST">
+        <div>
+          <label htmlFor="iemail">E-mail:</label>
+          <input type="email" id="iemail" name="email" />
+        </div>
+        <div>
+          <label htmlFor="ipassword">Senha:</label>
+          <input type="password" id="ipassword" name="password" />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,34 +1,86 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Register.css";
-import PostRegisterUser from '../../api/PostRegisterUser';
+import api from "../../api/Api";
 
 const Register = () => {
-  const [registerName, setRegisterName] = useState("");
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("")
+
+  const handleSubmitRegisterUser = async (e) => {
+    e.preventDefault();
+
+    console.log(name, email, password, confirmpassword);
+
+    try{
+
+      const response = await api.post(
+        "/auth/register",
+        JSON.stringify({ name, email, password, confirmpassword }),
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+        );
+
+        console.log(response.data);
+        setError('')
+      }catch(error){
+        if(!error?.response){
+          setError("Erro ao acessar o servidor");
+        }else if(error.response.status === 422){
+          setError(error.response.data.msg)
+        }
+      }
+  };
 
   return (
     <div className="container">
-      <form action="" method="POST">
+      <form onSubmit={handleSubmitRegisterUser}>
         <div>
           <label htmlFor="iname">Nome:</label>
-          <input type="text" id="iname" name="name"/>
+          <input
+            type="text"
+            id="iname"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
         </div>
         <div>
           <label htmlFor="iemail">E-mail:</label>
-          <input type="email" id="iemail" name="email"/>
+          <input
+            type="email"
+            id="iemail"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
         </div>
         <div>
           <label htmlFor="ipassword">Senha:</label>
-          <input type="password" id="ipassword" name="password"/>
+          <input
+            type="password"
+            id="ipassword"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
         </div>
         <div>
           <label htmlFor="iconfirmpassword">Confirmar Senha:</label>
-          <input type="password" id="iconfirmpassword" name="confirmpassword"/>
+          <input
+            type="password"
+            id="iconfirmpassword"
+            name="confirmpassword"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            value={confirmpassword}
+          />
         </div>
         <button type="submit">Cadastrar</button>
       </form>
+      <p>{error}</p>
     </div>
   );
 };
